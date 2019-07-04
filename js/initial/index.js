cb2953954c79b735fd2fb779760507d124929f46
BodhServant <51540912+BodhServant@users.noreply.github.com>
2019-07-04 20:12:27 +0700

    update

--- js/initial/index.js
+++ js/initial/index.js
@@ -100,7 +100,7 @@ ${lastNewsData.map(lastNews).join("")}
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
       plugins: [ 'interaction', 'dayGrid' ],
-      defaultDate: '2019-06-12',
+      defaultDate: '2019-07-12',
       editable: true,
       locale: 'th',    
       eventLimit: true, // allow "more" link when too many events
